15d9c707e522e10e713e4cad119783f6b525937c
qqibrow <qqibrow@gmail.com>
2016-03-11 00:18:08 -0800

    change to use nametuple vehicleIdentifier

--- sample/requestKbb.py
+++ sample/requestKbb.py
@@ -4,6 +4,14 @@ import requests
 from lxml import html
 import re
 import json
+from collections import namedtuple
+
+VehicleIdentifier = namedtuple("VehicleIdentifier", "brand year body_type")
+
+
+def categories_page(vehicle):
+    return "http://www.kbb.com/{0}/{1}/{2}/categories/".format(vehicle.brand, vehicle.body_type, vehicle.year)
+
 
 USER_AGENTS = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
                'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0',
@@ -22,25 +30,25 @@ def _request_page(link):
 brand_body_types_link = "http://www.kbb.com/jsdata/3.0.0.0_51577/_makesmodels?vehicleclass=UsedCar&yearid=2012"
 response = _request_page(brand_body_types_link)
 response_in_json = response.json()
-urls = []
+
+# Get all kinds of Vehicle Identifiers.
+vehicles = []
 for brand_category in response_in_json:
     name = brand_category["Name"].replace(' ', '-')
     body_types = [body_type['Name'] for body_type in brand_category['Model']]
-    urls_of_brand = ["http://www.kbb.com/{0}/{1}/{2}/categories/".format(name, body_type.replace(' ', '-'), "2012") for
-                     body_type in body_types]
-    urls.extend(urls_of_brand)
-# print '\n'.join(map(str, urls))
+    vehicles_of_this_brand = [VehicleIdentifier(brand=name, year=2012, body_type=body_type.replace(' ', '-')) for
+                              body_type in body_types]
+    vehicles.extend(vehicles_of_this_brand)
 
 # testing bmw links
-bmw_links = [link for link in urls if "BMW" in link]
-# print bmw_links
+bmw_links = [categories_page(link) for link in vehicles if "BMW" in link.brand]
 bmw_links = [bmw_links[0]]
 
 choose_style_links = []
 for bmw_link in bmw_links:
     result = _request_page(bmw_link)
     tree = html.fromstring(result.content)
-    style_links_of_brand = ["http://www.kbb.com/{}".format(a.get('href')) for a in
+    style_links_of_brand = ["http://www.kbb.com{}".format(a.get('href')) for a in
                             tree.cssselect('div .mod-category-inner a')] or [bmw_link]
     choose_style_links.extend(style_links_of_brand)
 
@@ -52,8 +60,6 @@ for choose_style_link in choose_style_links:
     # remove the page that customize the body type
     regular_option_links = filter(lambda link: "compare-styles" not in link, option_links)
     option_pages.extend(regular_option_links)
-
-print option_pages
 
 
 def _construct_detail_page(option_link):
@@ -76,6 +82,6 @@ for detail_page in detail_pages:
                           cookies={'PersistentZipCode': '94089'})
     matched = pattern.search(result.content)
     if matched:
-        print _get_price_from_matched_text(matched.group(1))
+        print detail_page, _get_price_from_matched_text(matched.group(1)), '\n'
 
 # task2. distribute version of request
