4812ad85923baf9d430d5f48b1ac2f0d99882869
qqibrow <qqibrow@gmail.com>
2016-03-12 23:33:52 -0800

    the return result canot be parsed from client side. need to change that in scraper

--- server/app/views.py
+++ server/app/views.py
@@ -1,11 +1,25 @@
 from app import app
 import redis
 import json
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/typeahead/<brand>')
+def get_typeahead(brand):
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    results = r.hgetall(brand)
+
+    # Turn results to a list.
+    results_in_json = json.dumps(results.keys())
+    names = []
+    for r in results_in_json:
+        a = json.loads(r)
+        names.append(a["model"])
+    return names
 
 @app.route('/vehicle_price/<brand>')
 def get_all_vehicle_of_brand(brand):
